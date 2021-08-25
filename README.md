9a4fc8779d827d2839cf975f5dc16c008d8cf940
Domagoj Satrapa <domsatrapa@gmail.com>
2021-08-25 15:24:40 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# TEABOT    
+
+A simple slack bot that responds to all support related messages outside of working hours.
+
+It also has the ability to set and get the active troubleshooter (person in charge of handling support related activities)
---
+++ bot_messages.py
@@ -0,0 +1,8 @@
+MSG_BOT_HELP = """I am *Locutus Bot*, 
+Here's what I can do:\n
+*@Locutus Bot _command_*\n
+`help` - print this message\n
+`start duty` - set yourself as troubleshooter\n
+`get duty` - prints who's the troubleshooter today\n
+"""
+MSG_TEAM_ABSENT = "This channel is monitored between 7:00AM and 3:00PM CET on working days. If it's urgent, please call the person on duty. The schedule is located <https://confluence.infobip.com/display/TT|*HERE*>.\nIf it's not urgent, please open a Jira task on the <https://jira.infobip.com/projects/LTT/issues/LTT-626?filter=allopenissues|*LTT*> project. We'll be on it as soon as possible."
