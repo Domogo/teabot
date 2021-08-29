273e2dc2bb961f374b8ca3e126e41973e17beb7e
Domagoj Satrapa <domsatrapa@gmail.com>
2021-08-29 14:11:54 +0200

    Update bot_messages.py

--- bot_messages.py
+++ bot_messages.py
@@ -5,4 +5,4 @@ Here's what I can do:\n
 `start duty` - set yourself as troubleshooter\n
 `get duty` - prints who's the troubleshooter today\n
 """
-MSG_TEAM_ABSENT = "This channel is monitored between 7:00AM and 3:00PM CET on working days. If it's urgent, please call the person on duty. The schedule is located <https://confluence.infobip.com/display/TT|*HERE*>.\nIf it's not urgent, please open a Jira task on the <https://jira.infobip.com/projects/LTT/issues/LTT-626?filter=allopenissues|*LTT*> project. We'll be on it as soon as possible."
+MSG_TEAM_ABSENT = "This channel is monitored between 7:00AM and 3:00PM CET on working days. If it's urgent, please call the person on duty."
